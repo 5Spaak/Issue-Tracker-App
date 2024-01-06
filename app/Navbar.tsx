07ce370f265b7d7f1b821d9db738fba53f6bcb9c
Spaak <spaakjobrighngoma@gmail.com>
2024-01-06 21:42:07 +0100

    Initial commit from Create Issue-tracker App

---
+++ app/Navbar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import {FaBug} from "react-icons/fa"
+import classNames from "classnames";
+
+const Navbar = () => {
+
+  const currentPage = usePathname()
+
+    const links = [
+        {label:'Dashboard', href:'/'},
+        {label:'Issues', href:'/issues'},
+    ]
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+        <Link href='/'><FaBug/></Link>
+        <ul className="flex space-x-6">
+           {
+            links.map(link => 
+                <Link 
+                    key={link.href} 
+                    className={classNames({
+                      'text-zinc-900': link.href===currentPage,
+                      'text-zinc-500': link.href!==currentPage,
+                      'hover:text-zinc-800 transition-colors':true
+                    })}
+                    href={link.href}>{link.label}</Link>)
+           }
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
